3d354fc20f4ee22dc48a7d5009fd9c6f90e55b70
Heino Pärnpuu <parnpuuheino@windowslive.com>
2019-09-18 13:02:41 +0300

    18.12

---
+++ ConsoleApp1/Food.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class Food
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Food(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+                
+    }
+}
---
+++ ConsoleApp1/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ShoppingCart ShoppingCart { get; set; }
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
