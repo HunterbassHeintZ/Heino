83a04d9e26e9e077f4290e64858b71c824251fc5
Heino Pärnpuu <heinoolen@gmail.com>
2019-10-02 13:26:15 +0300

    KT

--- TTHK/Program.cs
+++ TTHK/Program.cs
@@ -10,6 +10,73 @@ namespace TTHK
     {
         static void Main(string[] args)
         {
+            List<Teachers> Teachers = new List<Teachers>();
+
+            Console.WriteLine("Please enter the teachers first name!");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter the teachers last name");
+            string lastName = Console.ReadLine();            
+
+            Console.WriteLine("Is the teacher full time: True/False");
+            string isFullTime = Console.ReadLine();
+
+            Teachers teacher = new Teachers(firstName, lastName, isFullTime);
+
+            Teachers.Add(teacher);
+
+            Console.WriteLine("Do you want to add a course");
+
+            while (true)
+            {
+                if (Console.ReadLine() == "Y")
+                {
+                    Console.WriteLine("What courses does the teacher have");
+                    string courseName = Console.ReadLine();
+
+                    Console.WriteLine("How long are the courses");
+                    int duration = int.Parse(Console.ReadLine());
+
+                    Course course = new Course(courseName, duration);
+
+                    teacher.AddCourses(course);
+
+                    Console.WriteLine("Is there anything else? Y/N");
+                }
+
+                else
+                {
+                    break;
+                }
+            }            
+            
+            Console.WriteLine(teacher);
+
+            
+            Console.ReadKey();
+            
+        }
+        private static void CourseAdd()
+        {
+
+            if (Console.ReadLine() == "Y")
+            {
+                Console.WriteLine("What courses does the teacher have");
+                string courseName = Console.ReadLine();
+
+                Console.WriteLine("How long are the courses");
+                int duration = int.Parse(Console.ReadLine());
+
+                Course course = new Course(courseName, duration);
+
+                Console.WriteLine("Is there anything else? Y/N");
+            }
+
+            else
+            {
+                return;
+            }
+            
         }
     }
 }
--- TTHK/Teachers.cs
+++ TTHK/Teachers.cs
@@ -7,28 +7,28 @@ using System.Threading.Tasks;
 namespace TTHK
 {
     class Teachers
-    {        public string FirstName { get; set; }
+    {   public string FirstName { get; set; }
         public string LastName { get; set; }
-        public bool IsFullTime { get; set; }
+        public string IsFullTime { get; set; }
         public List<Course> Courses { get; set; }
 
-
-        public Teachers(string firstName, string lastName, bool isFullTime)
+        
+        public Teachers(string firstName, string lastName, string isFullTime)
         {
             FirstName = firstName;
             LastName = lastName;
             IsFullTime = isFullTime;
             Courses = new List<Course>();
         }
-
+        
         public void AddCourses(Course course)
         {
             Courses.Add(course);
-        }
+        }        
 
         public override string ToString()
         {
-            return $"{FirstName} {LastName}";
+            return $"{FirstName} {LastName} {IsFullTime} ";
         }
     }
 }
