d89a8fed54dc222bd5ecc894a01a0c3082bddc56
Heino Pärnpuu <heinoolen@gmail.com>
2019-10-02 11:19:52 +0300

    TTHK

---
+++ TTHK/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTHK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ TTHK/Teachers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTHK
+{
+    class Teachers
+    {        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsFullTime { get; set; }
+        public List<Course> Courses { get; set; }
+
+
+        public Teachers(string firstName, string lastName, bool isFullTime)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            IsFullTime = isFullTime;
+            Courses = new List<Course>();
+        }
+
+        public void AddCourses(Course course)
+        {
+            Courses.Add(course);
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+    }
+}
